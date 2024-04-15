48aa13a6cf7382c5a199e729519b97a274c18730
AvhiMaz <avhimazumder5@outlook.com>
2024-04-15 18:47:24 +0530

    fixed build error

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -9,24 +9,28 @@ const projects = [
     description:
       "Introducing my portfolio: a showcase of my skills, projects, and experiences. Explore my work and learn more about me through this curated collection of my achievements and endeavors.",
     link: "",
+    date: "",
   },
   {
     title: "Blogging App",
     description:
       "A blogging app: built with Next.js and Hashnode's headless CMS for seamless content management and dynamic frontend. It's the perfect platform for bloggers to share their thoughts effortlessly.",
     link: "https://github.com/AvhiMaz/hashnode-headless-CMS",
+    date: "",
   },
   {
     title: "Fitnesspergram",
     description:
       "FitnessPerGram's startup website: your destination for healthy food delivery. Discover our nutritious and delicious meals, crafted to support your fitness journey, all conveniently delivered to your doorstep.",
     link: "https://fitnesspergram.vercel.app/",
+    date: "",
   },
   {
     title: "Link Juntion",
     description:
       "LinkTree-inspired project: A minimalist solution for sharing multiple links effortlessly. Simplify your online presence and make navigation a breeze for your audience.",
     link: "https://links-junction.vercel.app/",
+    date: "",
   },
 
   {
@@ -34,12 +38,14 @@ const projects = [
     description:
       "Worked with the CNCF and Argo CD project to make error messages better. This helps people find and fix problems more easily. My work made the project stronger and better at handling errors.",
     link: "https://github.com/argoproj/argo-cd/pull/15059",
+    date: "",
   },
   {
     title: "Brew Setup Installer",
     description:
       "The brew-setup.sh script is designed to detect the Linux distribution type (Arch-based, Debian-based) and execute the appropriate commands for installing Homebrew.",
     link: "https://github.com/AvhiMaz/brew-setup",
+    date: "",
   },
 ];
 
