5a0765fcff7061bbecd42e166a97f23e529f54fc
AvhiMaz <avhimazumder5@outlook.com>
2024-04-23 13:12:10 +0530

    changes in button

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,6 @@
 import Link from "next/link";
 import React from "react";
 import Particles from "./components/particles";
-import { HoverBorderGradientDemo } from "./components/button";
 
 const navigation = [
   { name: "Projects", href: "/projects" },
@@ -42,10 +41,15 @@ export default function Home() {
           that solves problems and enhances user experiences with creativity and
           precision.
         </h2>
-        <div className="">
-          <Link href="/experience">
-            <HoverBorderGradientDemo />
-          </Link>
+        <div className="mt-10">
+          <a
+            href="/experience"
+            className="relative inline-flex items-center justify-center px-4 py-4 overflow-hidden font-mono font-medium tracking-tighter text-white bg-gray-800 rounded-lg group"
+          >
+            <span className="absolute w-0 h-0 transition-all duration-500 ease-out bg-green-500 rounded-full group-hover:w-56 group-hover:h-56"></span>
+            <span className="absolute inset-0 w-full h-full -mt-1 rounded-lg opacity-30 bg-gradient-to-b from-transparent via-transparent to-gray-700"></span>
+            <span className="relative">My Experiences</span>
+          </a>{" "}
         </div>
       </div>
     </div>
