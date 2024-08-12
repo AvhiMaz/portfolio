98abe8c0b74bb8b6aeff5567999a0403bf639568
AvhiMaz <avhimazumder5@outlook.com>
2024-08-12 16:48:01 +0530

    reload fix

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -4,6 +4,7 @@ import cors from "cors";
 import counterRoutes from "./routes/counter";
 import axios from "axios";
 import { config } from "./config/config";
+import globalErrorHandler from "./middleware/globalErrorHandler";
 
 const app = express();
 
@@ -11,6 +12,8 @@ app.use(express.json());
 app.use(cors());
 
 app.use("/api", counterRoutes);
+
+app.use(globalErrorHandler);
 
 const url = config.backendUrl as string;
 const interval = 30000;
