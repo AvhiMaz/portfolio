312b7ecea099c659f3ef5cb079d9aece6c37b7d1
AvhiMaz <avhimazumder5@outlook.com>
2024-08-12 16:37:58 +0530

    render hack

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -2,6 +2,8 @@
 import express from "express";
 import cors from "cors";
 import counterRoutes from "./routes/counter";
+import axios from "axios";
+import { config } from "./config/config";
 
 const app = express();
 
@@ -9,5 +11,28 @@ app.use(express.json());
 app.use(cors());
 
 app.use("/api", counterRoutes);
+
+const url = config.backendUrl as string;
+const interval = 30000;
+
+function reloadWebsite() {
+  axios
+    .get(url)
+    .then((response) => {
+      console.log(
+        `Reloaded at ${new Date().toISOString()}: Status Code ${
+          response.status
+        }`
+      );
+    })
+    .catch((error) => {
+      console.error(
+        `Error reloading at ${new Date().toISOString()}:`,
+        error.message
+      );
+    });
+}
+
+setInterval(reloadWebsite, interval);
 
 export default app;
--- backend/src/config/config.ts
+++ backend/src/config/config.ts
@@ -6,6 +6,7 @@ const _config = {
   port: process.env.PORT || 5513,
   nodeEnv: process.env.NODE_ENV || "development",
   databaseUrl: process.env.MONGODB_URI,
+  backendUrl: process.env.BACKEND_URL,
 };
 
 export const config = Object.freeze(_config);
