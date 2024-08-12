b93350087dfaa45daf02582400e0174264f6c363
AvhiMaz <avhimazumder5@outlook.com>
2024-08-12 10:05:19 +0530

    update counter endpoint

---
+++ app/api/viewcount/route.ts
@@ -0,0 +1,27 @@
+import dbConnect from "@/lib/dbConnet";
+import Counter from "@/model/counter";
+import { NextResponse } from "next/server";
+
+export async function GET() {
+  await dbConnect();
+
+  try {
+    const counter = await Counter.findOneAndUpdate(
+      { page: "profile" },
+      { $inc: { count: 1 } },
+      { new: true, upsert: true }
+    );
+
+    if (!counter) {
+      return NextResponse.json({ count: 0 });
+    }
+
+    return NextResponse.json({ count: counter.count });
+  } catch (error) {
+    console.error("Error updating counter:", error);
+    return NextResponse.json(
+      { error: "Failed to update counter" },
+      { status: 500 }
+    );
+  }
+}
