d4096b3b14ec52724f84190f99df2be6ba9b44cd
AvhiMaz <avhimazumder5@outlook.com>
2024-08-30 16:41:17 +0530

    update bio

--- frontend/app/components/footer.tsx
+++ frontend/app/components/footer.tsx
@@ -1,9 +1,10 @@
 export default function Footer() {
   return (
-    <footer className="mb-24 text-sm md:text-xl lg:text-xl md:mb-0 flex items-center justify-center">
-      <ul className="mt-5 flex items-center text-zinc-500 md:flex-row md:space-x-4 md:space-y-0">
-        full stack dev. 🤠
-      </ul>
+    <footer className="mb-20 text-sm md:text-xl lg:text-xl md:mb-0 flex items-center justify-center">
+      <p className="mt-5 flex mx-7 text-center justify-center items-center text-zinc-500">
+        Full stack developer crafting innovative solutions with a passion for
+        clean code.
+      </p>
     </footer>
   );
 }
